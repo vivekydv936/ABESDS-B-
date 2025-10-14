b8ad14f3951f2018720c4811f1d53d18641bae14
vivekydv936 <vivekydv936@gmail.com>
2025-10-14 11:04:14 +0530

    using DOM

---
+++ javascript/ES6/dom.js
@@ -0,0 +1,9 @@
+const div = document.createElement("div");
+const h1 =document.createElement("h1");
+h1.innerText ="Python is used to create Ai"
+div.appendChild(h1);
+const id=document.getElementById("root");
+id.appendChild(div);
+
+
+
